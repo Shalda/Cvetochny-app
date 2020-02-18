ec1485b21a716beea117416ee5e5fe4bc53130e0
shalda <zeppelin.ua@gmail.com>
2020-02-18 13:29:39 +0200

    pagination remove filter pipe

--- src/app/content/shop/right-cards/right-cards.component.ts
+++ src/app/content/shop/right-cards/right-cards.component.ts
@@ -59,11 +59,39 @@ export class RightCardsComponent implements OnInit {
         return  this.getAllproducts().slice(pageIndex, pageIndex + this.pageSize);
     }
     getAllproducts(): Product[] {
+        let products: Product[];
         if (this.selectedCategory === undefined) {
-            return this._repository.getProducts(this.parentCategory);
+            products = this._repository.getProducts(this.parentCategory);
         } else {
-            return this._repository.getProducts(this.parentCategory)
+            products = this._repository.getProducts(this.parentCategory)
                 .filter(p => p.subcategory === this.selectedCategory || p.category === this.selectedCategory);
+        }
+        return this.filterProducts(products);
+    }
+
+    filterProducts(products: Product[]): Product[] {
+        let sortedProducts: Product[] = [];
+        switch (this.orderSelector) {
+            case 'expensive':
+                sortedProducts = products.sort((a, b) => {
+                    return +b.price - +a.price;
+                });
+                break;
+            case 'cheap':
+                sortedProducts = products.sort((a, b) => {
+                    return +a.price - +b.price;
+                });
+                break;
+            case 'novelty':
+                sortedProducts = products.sort((a, b) => {
+                    return <any>new Date(b.create_ts) - <any>new Date(a.create_ts);
+                });
+                break;
+            default:
+                return products;
+        }
+        if (sortedProducts.length > 0) {
+            return sortedProducts;
         }
     }
 }
